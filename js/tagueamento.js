79d334c5b963e7d4204ccf9cc530084a421d2e87
CleberNandi <cleber.nandi@gmail.com>
2021-09-05 19:45:57 -0300

    Add function for event click on button menu 'Entre em Contato'

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -4,3 +4,16 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+function teste() {
+    console.log("Aqui");
+};
+
+var captureContact = function click_contact() {
+    console.log("Clieuqi aqui")
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'menu',
+        eventAction: 'entre_em_contato',
+        eventLabel: 'link_externo'
+    });
+}
