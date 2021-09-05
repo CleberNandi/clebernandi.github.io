65b064edbd0d1cc68b1475d1cc1f82019037e041
CleberNandi <cleber.nandi@gmail.com>
2021-09-05 18:02:46 -0300

    Modify local of tag scaning to page view para tagueamento.js

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -4,3 +4,5 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+gtag('config', 'UA-12345-6');
+gtag('config', 'UA-206684270-1');
